adb99b7c48f716aa281735bc0e48ac02d3612938
DeokJong <novelss444@jbnu.ac.kr>
2024-01-03 11:04:32 +0900

    Jin Done 7682 problem

--- 7682/main.cpp
+++ 7682/main.cpp
@@ -1,109 +1,125 @@
-#include <iostream>
 #include <string>
+#include <iostream>
+
 using namespace std;
+
 char** makeMatrix(string line);
-int getDoneCount(char** matrix);
-bool isVaildCount(string line);
+bool isXWin(char** matrix);
+bool isOWin(char** matrix);
+int getDiff(string line);
+bool isValid(char** mat,string line);
 bool isFullMatrix(string line);
 
-
-int main() {
+int main(){
+	char** mat;
 	string line;
+	
+
 	do {
 		getline(cin, line);
 		if (line == "end") break;
+		mat = makeMatrix(line);
 
-		char** matrix = makeMatrix(line);
+		if (isValid(mat, line))cout << "valid\n";
+		else cout << "invalid\n";
 
-		if (!isVaildCount(line)) printf("invalid\n");
-		else if(isFullMatrix(line))printf("valid\n");
-		else if(getDoneCount(matrix)==0)printf("invalid\n");
-		else printf("valid\n");
-
-		free(matrix);
+		for (int i = 0; i < 3; i++) {
+			delete[] mat[i];
+		}
+		delete[] mat;
 
 	} while (true);
 
 }
 
-
-char** makeMatrix(string line)
-{
+char** makeMatrix(string line) {
 	int lineCount = 0;
-
-	char** matrix = new char* [3];
-
+	char** mat = new char* [3];
 	for (int i = 0; i < 3; i++) {
-		matrix[i] = new char[3];
+		mat[i] = new char[3];
 	}
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			matrix[i][j] = line[lineCount];
-			lineCount++;
+			mat[i][j] = line[lineCount++];
 		}
 	}
-	
-	return matrix;
+
+	return mat;
 }
-
-int getDoneCount(char** matrix) {
-	int doneCount = 0;
-
-	for (int raw = 0; raw < 3; raw++){
-		if (matrix[raw][0] == matrix[raw][1] && matrix[raw][1] == matrix[raw][2] && matrix[raw][0] != '.') {
-			doneCount++;
+bool isXWin(char** matrix) {
+	for (int raw = 0; raw < 3; raw++) {//check colunm
+		if (matrix[raw][0] == 'X' &&
+			matrix[raw][0] == matrix[raw][1] &&
+			matrix[raw][1] == matrix[raw][2]) {
+			return true;
 		}
-	}//colunm check
-
-	for (int col = 0; col < 3; col++) {
-		if (matrix[col][0] == matrix[col][1] && matrix[col][1] == matrix[col][2] && matrix[col][0] != '.') {
-			doneCount++;
-		}
-	}//raw check
-
-	if (matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2] && matrix[0][0] != '.') {
-		doneCount++;
-	}
-	if (matrix[2][0] == matrix[1][1] && matrix[1][1] == matrix[0][2] && matrix[2][0] != '.') {
-		doneCount++;
 	}
 
-	return doneCount;
+	for (int col = 0; col < 3; col++) {//check raw
+		if (matrix[0][col] == 'X' &&
+			matrix[0][col] == matrix[1][col] && matrix[1][col] == matrix[2][col])return true;
+
+	}
+
+	/* check cross */ {
+		if (matrix[0][0] == 'X' &&
+			matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) return true;
+
+		if (matrix[0][2] == 'X' &&
+			matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) return true;
+	}
+
+	return false;
 }
+bool isOWin(char** matrix) {
+	for (int raw = 0; raw < 3; raw++) {//check colunm
+		if (matrix[raw][0] == 'O' &&
+			matrix[raw][0] == matrix[raw][1] &&
+			matrix[raw][1] == matrix[raw][2]) {
+			return true;
+		}
+	}
 
-bool isVaildCount(string line) {
-	int xCount=0;
+	for (int col = 0; col < 3; col++) {//check raw
+		if (matrix[0][col] == 'O' &&
+			matrix[0][col] == matrix[1][col] && matrix[1][col] == matrix[2][col])return true;
+
+	}
+
+	/* check cross */ {
+		if (matrix[0][0] == 'O' &&
+			matrix[0][0] == matrix[1][1] && matrix[1][1] == matrix[2][2]) return true;
+
+		if (matrix[0][2] == 'O' &&
+			matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0]) return true;
+	}
+
+	return false;
+
+}
+int getDiff(string line) {
+	int xCount = 0;
 	int oCount = 0;
-
 	for (int i = 0; i < 9; i++) {
-		switch (line[i])
-		{
-		case 'O':
-			oCount++;
-			break;
-		case 'X':
-			xCount++;
-			break;
-		case '.':
-			break;
-		}
+		if (line[i] == 'X') xCount++;
+		else if (line[i] == 'O')oCount++;
 	}
-
-	int diff = xCount - oCount;
-
-	if (0 == diff || diff == 1) {
-		return true;
-	}
-	else return false;
+	return xCount - oCount;
 }
+bool isValid(char** mat, string line) {
+	int diff = getDiff(line);
 
+	if (isXWin(mat) && isOWin(mat))return false;
+	if (isXWin(mat)&& diff == 1) return true;
+	if (isOWin(mat)&& diff == 0) return true;
+	if (isFullMatrix(line)&&diff==1&&!isXWin(mat)&&!isOWin(mat)) return true;
+
+	return false;
+}
 bool isFullMatrix(string line) {
-	for (int i = 0; i < 9; i++)
-	{
-		if (line[i] == 'O' || line[i] == 'X') continue;
-		return false;
+	for (int i = 0; i < 9; i++) {
+		if (line[i] == '.')return false;
 	}
-
 	return true;
 }
